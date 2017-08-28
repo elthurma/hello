2d7f8784bdd82896404db934fb6c85da60c3bc04
EllefThurmann <EllefThurmann@ELLEFSPC>
2017-08-28 13:26:49 +0200

    Initial commit :)

---
+++ HelloWorld/src/mypackage/Goodbye.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class Goodbye extends Hello {
+	public void hello() {
+		System.out.println("in GoodbyHello");
+		
+	}
+public static void main(String args[]) {
+	System.out.println("in main");
+	Hello gb1 = new Hello();
+	gb1.hello();
+	System.out.println("yeah");
+	
+}
+}
---
+++ HelloWorld/src/mypackage/Hello.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class Hello {
+public void hello() {System.out.println("In hello method");}
+public Hello() {System.out.println("In ctor");}
+	
+}
