0dd2b46ead9a24026a447735d1f708715e41901f
EllefThurmann <EllefThurmann@ELLEFSPC>
2017-08-28 14:33:48 +0200

    add main

---
+++ HelloWorld/src/mypackage/Main.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Main {
+
+	public static void main(String args[]) {
+		System.out.println("in main");
+		Hello gb1 = new Hello();
+		gb1.hello();
+		System.out.println("yeahyeahyah");
+	}
+
+}
